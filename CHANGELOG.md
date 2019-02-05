4997a5464a83bd6845accb0cab7548f3d711075b
Unknown <jsummers@obscuredetour.com>
2019-02-05 00:17:39 -0700

    1.1.2 - Markup complete, new JS, body scroll, default burger, green

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -8,6 +8,13 @@ All notable changes to this project will be documented in this file.
 - SCSS.
 - Keep logo on the same side as desktop?
 
+## [1.1.2] - 2019-02-04
+### Changed
+- Rewrote the close on click JS function.
+- Eliminiated body scroll when mobile menu is active. (not for iOS)
+- Rewrote burger squeeze and incorporated 'regular' burger style.
+- New colors, logo. Next are Readme and docs.
+
 ## [1.1.1] - 2019-02-03
 ### Added
 - Colors.
--- assets/js/nav.js
+++ assets/js/nav.js
@@ -1,42 +1,45 @@
 // This anonymous function can be inserted anywhere
 // (eg. within a <script> tag or anywhere in an existing .js file or its own file)
-(navDuty => {   
-  const sideNav = document.getElementById('sideNav');
-  const closeNavBtn = document.querySelector('.closeBtn');
-  const toggleNavBtn = document.querySelector('.toggleNav');
-  const pageOverlay = document.querySelector('.page__overlay');
+(navDuty => {
+  const sideNav = document.querySelector('.nav-list');
+  const toggleNavBtn = document.querySelector('.toggle-nav');
+  const burger = document.querySelector('.burger');
+  const pageOverlay = document.querySelector('.overlay');
+  const body = document.querySelector('body');
 
   // Nav listeners
-  closeNavBtn.addEventListener('click', toggleNav);
   toggleNavBtn.addEventListener('click', toggleNav);
   pageOverlay.addEventListener('click', toggleNav);
   // Nav funtion
   function toggleNav() {
-    sideNav.classList.toggle('open');
+    sideNav.classList.toggle('-open');
+    burger.classList.toggle('open');
+    body.classList.toggle('_overflowy-hidden');
+    // if (sideNav.classList.contains('-open')) {
+    //   sideNav.addEventListener('touchmove', (e) => { e.preventDefault() })
+    // }
   };
 
   // (on mobile) close nav menu when link is clicked
   // this is useful on mobile when clicking an anchor tag on the current page (eg. index.html#last-section)
-  let navItems = document.querySelectorAll(".nav__item");                     
-  for (var i = 0; i < navItems.length; i++) {
-    navItems[i].addEventListener('click', function (event) {
-      toggleNav();
-    });
-  }
+  let navLinks = document.querySelectorAll(".link");
+  navLinks.forEach(el => {
+    el.addEventListener('click', (event) => { toggleNav() });
+  });
 })();
 
 
 //non related - this can be deleted
 (stickyNav => {
-  const button = document.querySelector('button');
-  const onOffHTML = document.querySelector('.sticky-on-off');
+  const button = document.querySelector('.button-default');
+  const onOffHTML = document.querySelector('.sticky-text');
   const on = '<strong>On</strong>';
   const off = '<strong>Off</strong>';
   const navWrapper = document.querySelector('.nav-wrapper');
 
   button.addEventListener('click', toggleStickyNav => {
-    navWrapper.classList.toggle('sticky-nav');
-    (navWrapper.classList.contains('sticky-nav') ? onOffHTML.innerHTML = on : onOffHTML.innerHTML = off);
+    navWrapper.classList.toggle('-sticky');
+    (navWrapper.classList.contains('-sticky') ? onOffHTML.innerHTML = on : onOffHTML.innerHTML = off);
   });
 
 })();
